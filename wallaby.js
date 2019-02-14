61f25a2df3f5ddd30b2f823f09d3a1be62962a40
Niek Saarberg <n.saarberg@gmail.com>
2019-02-14 23:29:48 +0100

    Wallaby tweak to find files.

--- wallaby.js
+++ wallaby.js
@@ -1,29 +1,35 @@
-module.exports = wallaby => ({
-  files: [
-		'__mocks__/*.ts',
-    'src/**/*.snap',
-    'src/**/*.ts?(x)',
-    'tsconfig.json',
-    '!src/**/*.spec.ts?(x)',
-    'jest.config.js',
-  ],
-  tests: [
-    'src/**/*.spec.ts?(x)',
-  ],
-  env: {
-    type: 'node',
-    runner: 'node',
-  },
-  compilers: {
-    '**/*.ts?(x)': wallaby.compilers.typeScript({
-      isolatedModules: true,
-      module: 'commonjs',
-    }),
-  },
-  testFramework: 'jest',
-  setup: function(wallaby) {
-    const config = require('./jest.config');
-    Object.keys(config.moduleNameMapper).forEach(k => (config.moduleNameMapper[k] = config.moduleNameMapper[k].replace('<rootDir>', wallaby.localProjectDir)))
-    wallaby.testFramework.configure(config);
-  },
-});
+const path = require('path');
+
+module.exports = wallaby => {
+  process.env.NODE_PATH += path.delimiter + path.join(wallaby.projectCacheDir, 'src');
+
+  return {
+    files: [
+      '__mocks__/*.ts',
+      'src/**/*.snap',
+      'src/**/*.ts?(x)',
+      'tsconfig.json',
+      '!src/**/*.spec.ts?(x)',
+      'jest.config.js',
+    ],
+    tests: [
+      'src/**/*.spec.ts?(x)',
+    ],
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+    compilers: {
+      '**/*.ts?(x)': wallaby.compilers.typeScript({
+        isolatedModules: true,
+        module: 'commonjs',
+      }),
+    },
+    testFramework: 'jest',
+    setup: function(wallaby) {
+      const config = require('./jest.config');
+      Object.keys(config.moduleNameMapper).forEach(k => (config.moduleNameMapper[k] = config.moduleNameMapper[k].replace('<rootDir>', wallaby.localProjectDir)))
+      wallaby.testFramework.configure(config);
+    },
+  };
+};
