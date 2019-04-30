f154b8ec0b96fbc474876e2f9829f3d57792b571
Niek Saarberg <n.saarberg@gmail.com>
2019-04-30 17:18:18 +0200

    Updated dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file.ts',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file.ts',
     '\\.(css|less)$': '<rootDir>/__mocks__/file.ts',
   },
   modulePaths: ['src'],
