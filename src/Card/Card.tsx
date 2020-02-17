be47c48a61826611ddb4d2cf384840034f6f2bd5
Niek Saarberg <n.saarberg@gmail.com>
2020-02-17 17:19:50 +0100

    Click feedback for Card with onClick prop set

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 
 import './card.css';
 import { cx } from 'cx';
+import { Ripple } from 'Ripple';
 
 type Props = {
   children: React.ReactNode;
   shadow?: '0' | '1' | '2' | '3';
-  onClick?: (event: React.SyntheticEvent<HTMLDivElement, MouseEvent | KeyboardEvent>) => void;
+  // TODO Rename onClick to ..?
+  onClick?: (event: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>) => void;
 };
 
-const Card = ({ children, shadow = '0', onClick }: Props) => (
-  <div
-    className={cx('Card', `shadow-${shadow}`, onClick && 'is-clickable')}
-    tabIndex={onClick ? 0 : undefined}
-    onClick={onClick}
-    onKeyPress={event => {
-      if (onClick && (event.key === 'Enter' || event.key === ' ')) {
-        onClick(event);
-      }
-    }}
-  >
-    {children}
-  </div>
-);
+const Card = ({ children, shadow = '0', onClick }: Props) => {
+  const props = {
+    className: cx('Card', `shadow-${shadow}`, onClick && 'is-clickable'),
+  };
+
+  if (onClick) {
+    return (
+      <Ripple
+        renderAs="div"
+        tabIndex={onClick ? 0 : undefined}
+        onClick={onClick}
+        onKeyPress={event => {
+          if (onClick && (event.key === 'Enter' || event.key === ' ')) {
+            onClick(event);
+          }
+        }}
+        {...props}
+      >
+        {children}
+      </Ripple>
+    );
+  }
+
+  return <div {...props}>{children}</div>;
+};
 
 export default Card;
