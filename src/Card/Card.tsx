f4d375c4e5d30f2333d96930c66c4cdcd22ecfd1
Niek Saarberg <n.saarberg@gmail.com>
2020-02-17 16:50:25 +0100

    Card / Button / IconButton outlines

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 import './card.css';
+import { cx } from 'cx';
 
 type Props = {
   children: React.ReactNode;
   shadow?: '0' | '1' | '2' | '3';
-  onClick?: (event: React.SyntheticEvent<HTMLDivElement, MouseEvent>) => void;
+  onClick?: (event: React.SyntheticEvent<HTMLDivElement, MouseEvent | KeyboardEvent>) => void;
 };
 
 const Card = ({ children, shadow = '0', onClick }: Props) => (
-  <div className={`Card shadow-${shadow}`} onClick={onClick}>
+  <div
+    className={cx('Card', `shadow-${shadow}`, onClick && 'is-clickable')}
+    tabIndex={onClick ? 0 : undefined}
+    onClick={onClick}
+    onKeyPress={event => {
+      if (onClick && (event.key === 'Enter' || event.key === ' ')) {
+        onClick(event);
+      }
+    }}
+  >
     {children}
   </div>
 );
